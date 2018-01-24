632791eec3f0b1af0054ee4e9532078f46f000e2
Jason Down <jdown@igloosoftware.com>
2018-01-23 23:33:31 -0500

    Created IClassifier interface and BasicClassifier implementation.

---
+++ DigitsRecognizer/CSharp/BasicClassifier.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp
+{
+    public class BasicClassifier : IClassifier
+    {
+        private IEnumerable<Observation> _data;
+        private readonly IDistance _distance;
+
+        public BasicClassifier(IDistance distance)
+        {
+            _distance = distance;
+        }
+
+        public void Train(IEnumerable<Observation> trainingSet)
+        {
+            _data = trainingSet;
+        }
+
+        public string Predict(int[] pixels)
+        {
+            Observation currentBest = null;
+            var shortest = double.MaxValue;
+
+            foreach (var objs in _data)
+            {
+                var dist = _distance.Between(objs.Pixels, pixels);
+                if (dist < shortest)
+                {
+                    shortest = dist;
+                    currentBest = objs;
+                }
+            }
+
+            return currentBest.Label;
+        }
+    }
+}
--- DigitsRecognizer/CSharp/IDistance.cs
+++ DigitsRecognizer/CSharp/IDistance.cs
@@ -1,30 +1,7 @@
-﻿using System;
-
-namespace CSharp
+﻿namespace CSharp
 {
     public interface IDistance
     {
         double Between(int[] pixels1, int[] pixels2);
-    }
-
-    public class ManhattanDistance : IDistance
-    {
-        public double Between(int[] pixels1, int[] pixels2)
-        {
-            if (pixels1.Length != pixels2.Length)
-            {
-                throw new ArgumentException("Inconsistent image sizes.");
-            }
-
-            var length = pixels1.Length;
-            var distance = 0;
-
-            for (int i = 0; i < length; i++)
-            {
-                distance += Math.Abs((pixels1[i] - pixels2[i]));
-            }
-
-            return distance;
-        }
     }
 }
