b7faabddf8c766eda62d887b3dee4494e2244de3
Jason Down <jdown@igloosoftware.com>
2018-01-25 01:02:41 -0500

    Added F# project with a script equivalent to all the C# code.

---
+++ DigitsRecognizer/FSharp/Script.fsx
@@ -0,0 +1,43 @@
+﻿open System.IO
+
+type Observation = { Label : string; Pixels : int[] }
+
+let dataPath =  __SOURCE_DIRECTORY__ + @"..\..\Data"
+let trainingPath = dataPath + @"\trainingsample.csv"
+
+let toObservation (csv : string) =
+    let columns = csv.Split ','
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..] |> Array.map toObservation
+
+let manhattanDistance pixels1 pixels2 =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun (x,y) -> abs (x-y))
+    |> Array.sum
+
+let train (trainingset : Observation []) =
+    let classify (pixels : int[]) =
+        trainingset
+        |> Array.minBy (fun x -> manhattanDistance x.Pixels pixels)
+        |> fun x -> x.Label
+    classify
+
+let trainingData = reader trainingPath
+let classifier = train trainingData
+
+let validationPath = dataPath + @"\validationsample.csv"
+let validationData = reader validationPath
+
+let evaluate validationSet classifier =
+    validationSet
+    |> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+    |> fun x -> x*100.
+    |> printfn "Correctly classified: %.2f%%"
+
+// Test
+evaluate validationData classifier
