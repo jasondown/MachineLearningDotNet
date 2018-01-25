11175a7e8f467eea408a9daa2f9ef92c66b0f1c8
Jason Down <jdown@igloosoftware.com>
2018-01-24 23:47:29 -0500

    Added C# evaluator class.

---
+++ DigitsRecognizer/CSharp/Evaluator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp
+{
+    public class Evaluator
+    {
+        public static double Correct(IEnumerable<Observation> validationSet, IClassifier classifier)
+        {
+            return validationSet.Select(o => Score(o, classifier))
+                                .Average();
+        }
+
+        private static double Score(Observation observation, IClassifier classifer)
+        {
+            return classifer.Predict(observation.Pixels) == observation.Label ? 1.0 : 0.0;
+        }
+    }
+}
