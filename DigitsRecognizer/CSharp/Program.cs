a2fd938554e5bb5aaf2145abe8c65809f05e3199
Jason Down <jdown@igloosoftware.com>
2018-01-24 23:55:59 -0500

    Console program is using new Evaluator (C#) and correctly classifying 93.40% of handwritten digits.

--- DigitsRecognizer/CSharp/Program.cs
+++ DigitsRecognizer/CSharp/Program.cs
@@ -6,8 +6,18 @@ namespace CSharp
     {
         static void Main()
         {
-            var trainingPath = @"..\..\..\Data\trainingsample.csv";
+            const string dataPath = @"..\..\..\Data";
+
+            var trainingPath = $@"{dataPath}\trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
+            var classifier = new BasicClassifier(new ManhattanDistance());
+            classifier.Train(training);
+
+            var validationPath = $@"{dataPath}\validationsample.csv";
+            var validation = DataReader.ReadObservations(validationPath);
+
+            var correct = Evaluator.Correct(validation, classifier);
+            Console.WriteLine("Correctly classified: {0:P2}", correct);
 
             Console.ReadLine();
         }
