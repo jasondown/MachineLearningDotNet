03d576199196b69ebbe704f15f682933942b6e38
Jason Down <jdown@igloosoftware.com>
2018-01-25 22:45:38 -0500

    Created a C# functional style classifier that takes a func instead of an IDistance and hooked it up to the console program.

---
+++ DigitsRecognizer/CSharp/FunctionalExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    public class FunctionalExample : IClassifier
+    {
+        private IEnumerable<Observation> _data;
+
+        public FunctionalExample(Func<int[], int[], int> distance)
+        {
+            Distance = distance ?? throw new ArgumentNullException(nameof(distance));
+        }
+
+        public Func<int[], int[], int> Distance { get; }
+
+        public void Train(IEnumerable<Observation> trainingSet)
+        {
+            _data = trainingSet;
+        }
+
+        public string Predict(int[] pixels)
+        {
+            Observation currentBest = null;
+            var shortest = double.MaxValue;
+
+            foreach (Observation obs in _data)
+            {
+                var dist = Distance(obs.Pixels, pixels);
+                if (dist < shortest)
+                {
+                    shortest = dist;
+                    currentBest = obs;
+                }
+            }
+
+            return currentBest.Label;
+        }
+    }
+}
--- DigitsRecognizer/CSharp/Program.cs
+++ DigitsRecognizer/CSharp/Program.cs
@@ -7,10 +7,11 @@ namespace CSharp
         static void Main()
         {
             const string dataPath = @"..\..\..\Data";
+            var manhattan = new ManhattanDistance();
 
             var trainingPath = $@"{dataPath}\trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
-            var classifier = new BasicClassifier(new ManhattanDistance());
+            var classifier = new BasicClassifier(manhattan);
             classifier.Train(training);
 
             var validationPath = $@"{dataPath}\validationsample.csv";
@@ -18,6 +19,13 @@ namespace CSharp
 
             var correct = Evaluator.Correct(validation, classifier);
             Console.WriteLine("Correctly classified: {0:P2}", correct);
+
+            // Test functional style
+            int Dist(int[] x, int[] y) => (int) manhattan.Between(x, y);
+            var funcClassifier = new FunctionalExample(Dist);
+            funcClassifier.Train(training);
+            var functionalCorrect = Evaluator.Correct(validation, funcClassifier);
+            Console.WriteLine("Correctly classified (func): {0:P2}", functionalCorrect);
 
             Console.ReadLine();
         }
