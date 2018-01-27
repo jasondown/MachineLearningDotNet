68c4bf6072c994a13e0f1ccf93a9301425edb6ea
Jason Down <jdown@igloosoftware.com>
2018-01-27 00:14:57 -0500

    Finished Naive Bayes classifier.

--- SpamOrHam/SpamOrHam/NaiveBayes.fs
+++ SpamOrHam/SpamOrHam/NaiveBayes.fs
@@ -24,3 +24,37 @@ module Classifier =
         groups
         |> Array.maxBy (fun (label, group) -> score tokenized group)
         |> fst
+
+    let proportion count total = float count / float total
+    let laplace count total = float (count + 1) / float (total + 1)
+    
+    let countIn (group : TokenizedDoc seq) (token : Token) =
+        group 
+        |> Seq.filter (Set.contains token)
+        |> Seq.length
+
+    let analyze (group : TokenizedDoc seq) (totalDocs : int) (classificationTokens : Token Set) =
+        let groupSize = group |> Seq.length
+        let score token =
+            let count = countIn group token
+            laplace count groupSize
+        let scoredTokens = 
+            classificationTokens
+            |> Set.map (fun token -> token, score token)
+            |> Map.ofSeq
+        let groupProportion = proportion groupSize totalDocs
+
+        { Proprotion = groupProportion
+          TokenFrequencies = scoredTokens }
+
+    let learn (docs : ('a * string) []) (tokenizer : Tokenizer) (classificationTokens : Token set) =
+        let total = docs.Length
+        docs
+        |> Array.map (fun (label, docs) -> label, tokenizer docs)
+        |> Array.groupBy fst
+        |> Array.map (fun (label, group) -> label, group |> Array.map snd)
+        |> Array.map (fun (label, group) -> label, analyze group total classificationTokens)
+
+    let train (docs : ('a * string) []) (tokenizer : Tokenizer) (classificationTokens : Token Set) =
+        let groups = learn docs tokenizer classificationTokens
+        classify groups tokenizer
