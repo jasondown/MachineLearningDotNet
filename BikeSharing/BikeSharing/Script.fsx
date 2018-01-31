f92f61b25fdb88fd4cdf6cba207d32ce1affe9b6
Jason Down <jdown@igloosoftware.com>
2018-01-31 00:12:26 -0500

    Quick little chart to show bike usage over time.

--- BikeSharing/BikeSharing/Script.fsx
+++ BikeSharing/BikeSharing/Script.fsx
@@ -1,8 +1,14 @@
 ﻿#I @"..\packages\"
 #r @"FSharp.Data.2.4.4\lib\net45\FSharp.Data.dll"
+#r @"FSharp.Charting.0.91.1\lib\net45\FSharp.Charting.dll"
 
 open FSharp.Data
+open FSharp.Charting
 
 type Data = CsvProvider<"day.csv">
 let dataset = Data.GetSample()
 let data = dataset.Rows
+
+let all = 
+    Chart.Line [ for observation in data -> observation.Cnt ]
+    |> Chart.Show
