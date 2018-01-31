05c838beb69ba62924bedf74e0b6eaaed484568e
Jason Down <jdown@igloosoftware.com>
2018-01-31 00:22:46 -0500

    Compared daily bike usage with moving average of 7 days and 30 days.

--- BikeSharing/BikeSharing/Script.fsx
+++ BikeSharing/BikeSharing/Script.fsx
@@ -9,6 +9,22 @@ type Data = CsvProvider<"day.csv">
 let dataset = Data.GetSample()
 let data = dataset.Rows
 
+//---------- Show all bikes
 let all = 
     Chart.Line [ for observation in data -> observation.Cnt ]
+    |> Chart.Show
+
+//---------- Plot original bike counts with moving averages over 7 days and 30 days
+let count = data |> Seq.map (fun row -> float row.Cnt)
+
+let movingAvg n (series : float seq) =
+    series
+    |> Seq.windowed n
+    |> Seq.map (fun xs -> xs |> Seq.average)
+    |> Seq.toList
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line (movingAvg 7 count)
+    Chart.Line (movingAvg 30 count) ]
     |> Chart.Show
