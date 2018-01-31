8e2500dfb80e2756cb980beb2e6e989ac3c9ff04
Jason Down <jdown@igloosoftware.com>
2018-01-31 00:04:33 -0500

    Loaded csv dataset.

--- BikeSharing/BikeSharing/Script.fsx
+++ BikeSharing/BikeSharing/Script.fsx
@@ -1,1 +1,8 @@
-﻿
+﻿#I @"..\packages\"
+#r @"FSharp.Data.2.4.4\lib\net45\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Data = CsvProvider<"day.csv">
+let dataset = Data.GetSample()
+let data = dataset.Rows
