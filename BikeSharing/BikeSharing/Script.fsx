8dd7482154efd6248e8bf35f624f3dacfb435189
Jason Down <jdown@igloosoftware.com>
2018-01-31 23:39:06 -0500

    Implemented and tested out a stochastic gradient descent algorithm.

--- BikeSharing/BikeSharing/Script.fsx
+++ BikeSharing/BikeSharing/Script.fsx
@@ -62,3 +62,33 @@ let cost (data : Obs seq) (m : Model) =
 let overallCost = cost data
 overallCost model0 |> printfn "Cost model0: %.0f"
 overallCost model1 |> printfn "Cost model1: %.0f"
+
+let update alpha (theta0, theta1) (obs : Obs) =
+    let y = float obs.Cnt
+    let x = float obs.Instant
+    let theta0' = theta0 - 2. * alpha * 1. * (theta0 + theta1 * x - y)
+    let theta1' = theta1 - 2. * alpha * x * (theta0 + theta1 * x - y)
+    theta0', theta1'
+
+let obs100 = data|> Seq.item 100
+let testUpdate = update 0.00001 (0., 0.) obs100
+cost [obs100] (model (0., 0.))
+cost [obs100] (model testUpdate)
+
+let stochastic rate (theta0, theta1) =
+    data
+    |>Seq.fold (fun (t0, t1) obs ->
+        printfn "%.4f, %.4f" t0 theta1
+        update rate (t0, t1) obs) (theta0, theta1)
+
+let tuneRate =
+    [ for r in 1 .. 20 ->
+        (pown 0.1 r), stochastic (pown 0.1 r) (0., 0.) |> model |> overallCost ]
+
+let rate = pown 0.1 8
+let model2 = model (stochastic rate (0.0, 0.0))
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line [ for obs in data -> model2 obs ] ] 
+    |> Chart.Show
