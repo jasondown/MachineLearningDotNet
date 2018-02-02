d3acadd5577bbd4baecac3b10378bc35149418c8
Jason Down <jdown@igloosoftware.com>
2018-02-01 23:51:11 -0500

    Added new script file to play with Linear Algebra. Also added nugetp package for MathNet.Numerics.

---
+++ BikeSharing/BikeSharing/LinearAlgebra.fsx
@@ -0,0 +1,10 @@
+﻿#I @"..\packages\"
+#r @"FSharp.Charting.0.91.1\lib\net45\FSharp.Charting.dll"
+#r @"FSharp.Data.2.4.4\lib\net45\FSharp.Data.dll"
+#r @"MathNet.Numerics.3.20.2\lib\net40\MathNet.Numerics.dll"
+
+open FSharp.Charting
+open FSharp.Data
+open MathNet
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
