9ffb738b91a24587dbdae93e6cd4929538d0eef5
Jason Down <jdown@igloosoftware.com>
2018-02-03 23:28:53 -0500

    Load headers and observations from the dataset.

--- Unsupervised/Unsupervised/Script.fsx
+++ Unsupervised/Unsupervised/Script.fsx
@@ -1,1 +1,17 @@
-﻿
+﻿open System
+open System.IO
+
+let folder = __SOURCE_DIRECTORY__
+let file = "userprofiles-toptags.txt"
+
+let headers, observations =
+    let raw = Path.Combine(folder, file) |> File.ReadAllLines
+
+    let headers = (raw.[0].Split ',').[1..] // skip user id column
+
+    let observations =
+        raw.[1..]
+        |> Array.map (fun line -> (line.Split ',').[1..])
+        |> Array.map (Array.map float)
+
+    headers, observations
