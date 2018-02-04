297199f3e26b84cef67306231578049cd09e1db3
Jason Down <jdown@igloosoftware.com>
2018-02-03 23:59:32 -0500

    Started a KMeans module for K Means Cluserting.

---
+++ Unsupervised/Unsupervised/KMeans.fs
@@ -0,0 +1,12 @@
+﻿namespace Unsupervised
+
+module KMeans =
+    
+    let pickFrom size k =
+        let rng = System.Random()
+        let rec pick (set : int Set) =
+            let candidate = rng.Next(size)
+            let set = set.Add candidate
+            if set.Count = k then set
+            else pick set
+        pick Set.empty |> Set.toArray
