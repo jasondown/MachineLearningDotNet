07b267833dab90f92f4993c36beb8209c38acdb2
Jason Down <jdown@igloosoftware.com>
2018-02-04 00:02:53 -0500

    Added KMeans.initialize function

--- Unsupervised/Unsupervised/KMeans.fs
+++ Unsupervised/Unsupervised/KMeans.fs
@@ -10,3 +10,17 @@ module KMeans =
             if set.Count = k then set
             else pick set
         pick Set.empty |> Set.toArray
+
+    let initialize observations k = 
+        let size = Array.last observations
+
+        let centroids = 
+            pickFrom size k
+            |> Array.mapi (fun i index ->
+                i+1, observations.[index])
+
+        let assignments =
+            observations
+            |> Array.map (fun x -> 0, x)
+
+        assignments, centroids
