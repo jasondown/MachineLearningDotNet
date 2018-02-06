beeb09d03640051680159d3e30c961b6c1ded00a
Jason Down <jdown@igloosoftware.com>
2018-02-05 23:34:14 -0500

    Added covariance matrix function.

--- Unsupervised/Unsupervised/PCA.fs
+++ Unsupervised/Unsupervised/PCA.fs
@@ -1,2 +1,18 @@
-﻿module PCA
+﻿namespace Unsupervised
 
+module PCA =
+
+    open MathNet
+    open MathNet.Numerics.LinearAlgebra
+    open MathNet.Numerics.Statistics
+
+    let covarianceMatrix (M : Matrix<float>) =
+        let cols = M.ColumnCount
+        let C = DenseMatrix.create cols cols Matrix.Zero
+        for c1 in 0 .. (cols - 1) do
+            C.[c1, c1] <- Statistics.Variance (M.Column c1)
+            for c2 in (c1 + 1) .. (cols - 1) do
+                let cov = Statistics.Covariance (M.Column c1, M.Column c2)
+                C.[c1, c2] <- cov
+                C.[c2, c1] <- cov
+        C
