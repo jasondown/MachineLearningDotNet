6ced82bf83a3301a8b973792df6a289032fb7044
Jason Down <jdown@igloosoftware.com>
2018-02-05 23:11:04 -0500

    Created a correlation matrix for tags that are similar.

--- Unsupervised/Unsupervised/PCA.fsx
+++ Unsupervised/Unsupervised/PCA.fsx
@@ -1,1 +1,43 @@
-﻿
+﻿#I @"..\packages"
+#r @"MathNet.Numerics.3.20.2\lib\net40\MathNet.Numerics.dll"
+#r @"MathNet.Numerics.FSharp.3.20.2\lib\net40\MathNet.Numerics.FSharp.dll"
+
+open MathNet
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.Statistics
+
+open System
+open System.IO
+
+let folder = __SOURCE_DIRECTORY__
+let file = "userprofiles-toptags.txt"
+
+let headers, observations =
+    let raw = Path.Combine(folder, file) |> File.ReadAllLines
+
+    let headers = (raw.[0].Split ',').[1..] // skip user id column
+
+    let observations =
+        raw.[1..]
+        |> Array.map (fun line -> (line.Split ',').[1..])
+        |> Array.map (Array.map float)
+
+    headers, observations
+
+let correlations = 
+    observations
+    |> Matrix.Build.DenseOfColumnArrays
+    |> Matrix.toRowArrays
+    |> Correlation.PearsonMatrix
+
+let features = headers.Length
+let correlated = 
+    [
+        for col in 0 .. (features - 1) do
+            for row in (col + 1) .. (features - 1) ->
+                correlations.[col, row], headers.[col], headers.[row]
+    ]   
+    |> Seq.sortByDescending (fun (corr, f1, f2) -> corr)
+    |> Seq.take 20
+    |> Seq.iter (fun (corr, f1, f2) ->
+        printfn "%s %s : %.2f" f1 f2 corr)
