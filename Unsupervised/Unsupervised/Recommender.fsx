ecdc09bd789ec4fe9274c7e3012c098e1f07aa7b
Jason Down <jdown@igloosoftware.com>
2018-02-06 23:55:30 -0500

    Created new recommender script file.

---
+++ Unsupervised/Unsupervised/Recommender.fsx
@@ -0,0 +1,17 @@
+﻿open System
+open System.IO
+
+let folder = __SOURCE_DIRECTORY__
+let file = "userprofiles-toptags.txt"
+
+let headers, observations =
+    let raw = Path.Combine(folder, file) |> File.ReadAllLines
+
+    let headers = (raw.[0].Split ',').[1..] // skip user id column
+
+    let observations =
+        raw.[1..]
+        |> Array.map (fun line -> (line.Split ',').[1..])
+        |> Array.map (Array.map float)
+
+    headers, observations
