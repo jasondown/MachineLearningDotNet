d5bfba532331bbbbd8c878632f3c1187877823a5
Jason Down <jdown@igloosoftware.com>
2018-02-07 17:35:38 -0500

    Added FSharp.Data nuget package and referenced it in the script.

--- Titanic/Titanic/Script.fsx
+++ Titanic/Titanic/Script.fsx
@@ -1,8 +1,3 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
-open Titanic
-
-// Define your library scripting code here
+﻿#I @"..\packages"
+#r @"FSharp.Data.2.4.4\lib\net45\FSharp.Data.dll"
 
