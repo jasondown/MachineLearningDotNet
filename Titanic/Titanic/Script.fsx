614dfad86e07389deaae7e7d91af703f31f3c4ab
Jason Down <jdown@igloosoftware.com>
2018-02-08 22:46:23 -0500

    Updated entropy to take a label. Created splitEntropy (average entropy after feature split).

--- Titanic/Titanic/Script.fsx
+++ Titanic/Titanic/Script.fsx
@@ -150,9 +150,23 @@ dataset.Rows
     if p.Survived = updatedClassifier p then 1.0 else 0.0)
 
 //----------Shannon Entropy
-let entropy data =
+let entropy label data =
     let size = data |> Seq.length
     data
-    |> Seq.countBy id
+    |> Seq.countBy label
     |> Seq.map (fun (_, count) -> float count / float size)
     |> Seq.sumBy (fun f -> if f > 0. then - f * log f else 0.)
+
+//---------- Average entropy after feature split
+let splitEntropy extractLabel extractFeature data =
+    let dataWithValues =
+        data
+        |> Seq.filter (extractFeature |> hasData)
+    let size = dataWithValues |> Seq.length
+    dataWithValues
+    |> Seq.groupBy extractFeature
+    |> Seq.sumBy (fun (_, group) ->
+        let groupSize = group |> Seq.length
+        let probaGroup = float groupSize / float size
+        let groupEntropy = group |> entropy extractLabel
+        probaGroup * groupEntropy)
