07716314980d0a12dbb96f5fd993d6966adff75d
Jason Down <jdown@igloosoftware.com>
2018-02-10 00:01:17 -0500

    Able to construct trees now. Library is usable. Will test in a script file.

--- Titanic/Titanic/Tree.fs
+++ Titanic/Titanic/Tree.fs
@@ -1,7 +1,6 @@
 ﻿namespace Titanic
 
 module Tree =
-    open FSharp.Data.HtmlAttribute
     
     type Feature<'a> = 'a -> string Option
     type NamedFeature<'a> = string * Feature<'a>
@@ -25,3 +24,60 @@ module Tree =
                     | Some _ -> value
             let nextLevelTree = branches.[usedValue]
             decide nextLevelTree observation
+
+    //---------- Growing a tree from a sample and features
+
+    let entropy label data = 
+        let size = data |> Seq.length
+        data 
+        |> Seq.countBy label 
+        |> Seq.map (fun (_, count) -> float count / float size)
+        |> Seq.sumBy (fun f -> if f > 0. then - f * log f else 0.)
+
+    let splitEntropy label feature data = 
+        let knownData = 
+            data 
+            |> Seq.filter (fun obs -> feature obs |> Option.isSome)
+        let size = knownData |> Seq.length
+        knownData 
+        |> Seq.groupBy feature
+        |> Seq.sumBy (fun (_, group) ->
+            let groupSize = group |> Seq.length
+            let groupEntropy = group |> entropy label
+            (float groupSize / float size) * groupEntropy) 
+
+    let mostFrequentBy f sample =
+        sample
+        |> Seq.map f
+        |> Seq.countBy id
+        |> Seq.maxBy snd
+        |> fst
+
+    let rec growTree sample label features = 
+
+        if (Map.isEmpty features)
+        then sample |> mostFrequentBy label |> Answer
+        else
+            let (bestName,bestFeature) = 
+                features
+                |> Seq.minBy (fun kv -> 
+                    splitEntropy label kv.Value sample)
+                |> (fun kv -> kv.Key, kv.Value)
+            let branches = 
+                sample
+                |> Seq.groupBy bestFeature
+                |> Seq.filter (fun (value, group) -> value.IsSome)
+                |> Seq.map (fun (value, group) -> value.Value,group)
+            let defaultValue = 
+                branches 
+                |> Seq.maxBy (fun (value, group) -> 
+                    group |> Seq.length)
+                |> fst
+            let remainingFeatures = features |> Map.remove bestName
+            let nextLevel = 
+                branches 
+                |> Seq.map (fun (value, group) -> 
+                    value, growTree group label remainingFeatures)
+                |> Map.ofSeq
+
+            Stump((bestName, bestFeature), defaultValue, nextLevel)
