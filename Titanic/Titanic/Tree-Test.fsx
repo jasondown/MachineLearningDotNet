be46d16fbff5975a10f79fcda2b85fd0d52fdcd0
Jason Down <jdown@igloosoftware.com>
2018-02-10 00:11:07 -0500

    Created new tree testing script. Loaded csv data into it and Tree library code.

---
+++ Titanic/Titanic/Tree-Test.fsx
@@ -0,0 +1,14 @@
+﻿#I @"..\packages"
+#r @"FSharp.Data.2.4.4\lib\net45\FSharp.Data.dll"
+#load "Tree.fs"
+
+open System
+open FSharp.Data
+open Titanic
+open Titanic.Tree
+
+type Titanic = CsvProvider<"titanic.csv">
+type Passenger = Titanic.Row 
+
+let dataset = Titanic.GetSample()
+
