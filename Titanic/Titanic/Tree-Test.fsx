4086442c761ce1ef707d3d4d95433c5b32748155
Jason Down <jdown@igloosoftware.com>
2018-02-10 00:16:52 -0500

    Creating a tree, with 80% accuracy!

--- Titanic/Titanic/Tree-Test.fsx
+++ Titanic/Titanic/Tree-Test.fsx
@@ -12,3 +12,14 @@ type Passenger = Titanic.Row
 
 let dataset = Titanic.GetSample()
 
+let label (p : Passenger) = p.Survived
+
+let features = [
+    "Sex", fun (p : Passenger) -> p.Sex |> Some
+    "Class", fun p -> p.Pclass |> string |> Some
+    "Age", fun p -> if p.Age < 7.0 then Some "Younger" else Some "Older" ]
+
+let tree = growTree dataset.Rows label (features |> Map.ofList)
+
+dataset.Rows
+|> Seq.averageBy (fun p -> if p.Survived = decide tree p then 1. else 0.)
