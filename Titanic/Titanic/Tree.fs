44533b337d0cd3681b15f23308a0769c639c0fd7
Jason Down <jdown@igloosoftware.com>
2018-02-09 23:47:15 -0500

    Created a Tree type along with some supporting types.

--- Titanic/Titanic/Tree.fs
+++ Titanic/Titanic/Tree.fs
@@ -2,4 +2,10 @@
 
 module Tree =
     
+    type Feature<'a> = 'a -> string Option
+    type NamedFeature<'a> = string * Feature<'a>
+    type Label<'a, 'b when 'b:equality> = 'a -> 'b
 
+    type Tree<'a, 'b when 'b:equality> =
+        | Answer of 'b
+        | Stump of NamedFeature<'a> * string * Map<string, Tree<'a, 'b>>
