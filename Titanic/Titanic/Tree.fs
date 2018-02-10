6ad040da8a12ddc0a01f80a370c54328aef9af83
Jason Down <jdown@igloosoftware.com>
2018-02-09 23:51:53 -0500

    Created a decision function for the tree.

--- Titanic/Titanic/Tree.fs
+++ Titanic/Titanic/Tree.fs
@@ -1,6 +1,7 @@
 ﻿namespace Titanic
 
 module Tree =
+    open FSharp.Data.HtmlAttribute
     
     type Feature<'a> = 'a -> string Option
     type NamedFeature<'a> = string * Feature<'a>
@@ -9,3 +10,18 @@ module Tree =
     type Tree<'a, 'b when 'b:equality> =
         | Answer of 'b
         | Stump of NamedFeature<'a> * string * Map<string, Tree<'a, 'b>>
+
+    let rec decide tree observation =
+        match tree with
+        | Answer labelValue -> labelValue
+        | Stump ((featureName, feature), valueWhenMissing, branches) ->
+            let featureValue = feature observation
+            let usedValue =
+                match featureValue with
+                | None -> valueWhenMissing
+                | Some value -> 
+                    match (branches.TryFind value) with
+                    | None -> valueWhenMissing
+                    | Some _ -> value
+            let nextLevelTree = branches.[usedValue]
+            decide nextLevelTree observation
