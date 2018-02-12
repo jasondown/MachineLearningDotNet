d8d3c491b307610203baf3473d9aee4dbce6cdd4
Jason Down <jdown@igloosoftware.com>
2018-02-11 22:14:23 -0500

    Functions created to grow a forest from trees.

--- Titanic/Titanic/Tree.fs
+++ Titanic/Titanic/Tree.fs
@@ -1,6 +1,7 @@
 ﻿namespace Titanic
 
 module Tree =
+    open System
     
     type Feature<'a> = 'a -> string Option
     type NamedFeature<'a> = string * Feature<'a>
@@ -137,3 +138,52 @@ module Tree =
                 |> Map.ofSeq
 
             Stump((bestName, bestFeature), defaultValue, nextLevel)
+
+    //----------Forest:
+    //               Random feature (without repitition)
+    //               Random examples from sample (with repitition)
+    //               Combine trees into majority vote
+    let pickRepeat (rng : Random) proportion original =
+        let size = original |> Array.length
+        let sampleSize = proportion * float size |> int
+        Array.init sampleSize (fun _ -> original.[rng.Next(size)])
+
+    let pickNoRepeat (rng : Random) proportion original =
+        let size = original |> Seq.length
+        let sampleSize = proportion * float size |> int
+
+        let init = ([], size)
+        original
+        |> Seq.fold (fun (sampled, remaining) item ->
+            let picked = List.length sampled
+            let p = float (sampleSize - picked) / float remaining
+            if (rng.NextDouble() <= p)
+            then (item::sampled, remaining - 1)
+            else (sampled, remaining - 1)) init
+        |> fst
+
+    let predict forest observation =
+        forest
+        |> Seq.map (fun tree -> decide tree observation)
+        |> Seq.countBy id
+        |> Seq.maxBy snd
+        |> fst
+
+    let growForest size sample label features =
+        let rng = Random ()
+
+        let propFeatures =
+            let total = features |> Seq.length |> float
+            sqrt total / total
+
+        let featSample () = pickNoRepeat rng propFeatures features
+        let popSample () = pickRepeat rng 1.0 sample
+        let filters = [ leafSizeFilter 10; entropyGainFilter ]
+
+        let forest = [
+            for _ in 1 .. size ->
+                let sample = popSample ()
+                let features = featSample () |> Map.ofList
+                growTree2 filters sample label features ]
+
+        predict forest
